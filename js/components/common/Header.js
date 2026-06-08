0e726362d62733e4531abaf1d01f00a29920c56e
김태은 <kbo6924@naver.com>
2026-06-08 09:27:27 +0900

    재커멋

--- js/components/common/Header.js
+++ js/components/common/Header.js
@@ -23,8 +23,10 @@ export default {
                 <!-- Header nav -->
                 <nav class="gnb-nav" aria-label="주요 메뉴">
                     <ul class="nav-ul">
-                        <li class="nav-li contact">
+                    <li class="nav-li contact">
                             <a  class="nav-link" href="https://app.notion.com/p/Git-git-github-37431ef0839480d19504e642e84443f4?v=d83e7c2af2484bfc9ccdbd4071d79068&source=copy_link" target="_blank" rel="noopener noreferrer" title="노션 새창열림"> <span>노션</span></a>
+                        </li>
+                        <li class="nav-li contact">
                             <button type="button" href="#void" @click="$emit('open-contact')" class="nav-link">
                                 <span>연락</span>
                             </button>
